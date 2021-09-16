9a8ff31b2e90dd3a988e49f52895a26808b9beb4
Juruel Keanu Lorenzo <39931559+JKLorenzo@users.noreply.github.com>
2021-09-16 18:21:17 +0800

    feat(constants): add role ids

--- src/utils/contants.ts
+++ src/utils/contants.ts
@@ -3,6 +3,13 @@ export default {
   roles: {
     lead: '884285001368223864',
     core: '884284934938820649',
+    vice_president: '888004869707948052',
+    secretary: '888004946027491328',
+    treasurer: '885165866101112882',
+    public_relations_manager: '888004990625533992',
+    public_image_committee: '885165634277744711',
+    social_media_committee: '888005107583688735',
+    technical_committee: '885165191258599455',
     bots: '884285921766305802',
     member: '884284885257289778',
   },
