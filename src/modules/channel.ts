603c77c9c43088cf76bc5d643cc729fc05a6c8c7
Juruel Keanu Lorenzo <39931559+JKLorenzo@users.noreply.github.com>
2021-09-16 17:00:30 +0800

    refactor: use queuer instead of queuerOf

--- src/modules/channel.ts
+++ src/modules/channel.ts
@@ -1,17 +1,19 @@
 import { Guild, GuildChannel, GuildChannelCreateOptions } from 'discord.js';
-import { queuerOf } from '../utils/queuer.js';
+import Queuer from '../utils/queuer.js';
+
+const queuer = new Queuer(500);
 
 type createOptions = { name: string } & GuildChannelCreateOptions;
 
 export function createChannel(guild: Guild, data: createOptions): Promise<GuildChannel> {
-  return queuerOf(guild.id).queue(async () => {
+  return queuer.queue(async () => {
     const channel = await guild.channels.create(data.name, data);
     return channel;
   });
 }
 
 export function deleteChannel(channel: GuildChannel): Promise<void> {
-  return queuerOf(channel.guildId).queue(async () => {
+  return queuer.queue(async () => {
     if (channel.deletable) await channel.delete();
   });
 }
--- src/modules/role.ts
+++ src/modules/role.ts
@@ -1,10 +1,11 @@
 import { CreateRoleOptions, Guild, GuildMember, Role } from 'discord.js';
-import { queuerOf } from '../utils/queuer.js';
+import Queuer from '../utils/queuer';
 
 const maxRoles = 250;
+const queuer = new Queuer(500);
 
 export function createRole(guild: Guild, data: CreateRoleOptions): Promise<Role | undefined> {
-  return queuerOf(guild.id).queue(async () => {
+  return queuer.queue(async () => {
     if (guild.roles.cache.size >= maxRoles) return;
     const role = await guild.roles.create(data);
     return role;
@@ -12,19 +13,19 @@ export function createRole(guild: Guild, data: CreateRoleOptions): Promise<Role 
 }
 
 export function deleteRole(role: Role): Promise<void> {
-  return queuerOf(role.guild.id).queue(async () => {
+  return queuer.queue(async () => {
     await role.delete();
   });
 }
 
 export function addRole(member: GuildMember, role: Role | Role[]): Promise<void> {
-  return queuerOf(member.guild.id).queue(async () => {
+  return queuer.queue(async () => {
     await member.roles.add(role);
   });
 }
 
 export function removeRole(member: GuildMember, role: Role | Role[]): Promise<void> {
-  return queuerOf(member.guild.id).queue(async () => {
+  return queuer.queue(async () => {
     await member.roles.remove(role);
   });
 }
